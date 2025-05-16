d0caa1c2ea8e02094bf9364c4546eaf29f0399f8
rakun256 <emreuslu256@gmail.com>
2025-05-16 23:20:01 +0300

    WeatherQuery finished.

--- src/main/java/com/rakun/weatherapp/entity/WeatherQuery.java
+++ src/main/java/com/rakun/weatherapp/entity/WeatherQuery.java
@@ -1,4 +1,31 @@
 package com.rakun.weatherapp.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "weather_queries")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class WeatherQuery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+
+    private String countryCode;
+
+    @Column(columnDefinition = "TEXT")
+    private String weatherJson;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
 }
