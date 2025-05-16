6c43776aaebc85cbcec5ee8e5b0d29ddf952bc2c
rakun256 <emreuslu256@gmail.com>
2025-05-16 23:31:03 +0300

    JwtResponse DTO is finished.

---
+++ src/main/java/com/rakun/weatherapp/dto/JwtResponse.java
@@ -0,0 +1,12 @@
+package com.rakun.weatherapp.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class JwtResponse {
+    private String token;
+}
