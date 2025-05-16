44195f535d06da25aa21c773aa374bc96b83f6b3
rakun256 <emreuslu256@gmail.com>
2025-05-16 23:26:44 +0300

    Repositories are finished.

---
+++ src/main/java/com/rakun/weatherapp/repository/WeatherQueryRepository.java
@@ -0,0 +1,9 @@
+package com.rakun.weatherapp.repository;
+
+import com.rakun.weatherapp.entity.WeatherQuery;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface WeatherQueryRepository extends JpaRepository<WeatherQuery, Long> {
+    List<WeatherQuery> findByCity(String city);
+}
---
+++ src/main/java/com/rakun/weatherapp/util/Role.java
@@ -0,0 +1,6 @@
+package com.rakun.weatherapp.util;
+
+public enum Role {
+    USER,
+    ADMIN
+}
