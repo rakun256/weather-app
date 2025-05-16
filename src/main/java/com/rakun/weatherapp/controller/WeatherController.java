fc4edc1be11312afec964d3635d0c1218328cb4d
rakun256 <emreuslu256@gmail.com>
2025-05-17 00:11:11 +0300

    WeatherController Created

---
+++ src/main/java/com/rakun/weatherapp/controller/WeatherController.java
@@ -0,0 +1,16 @@
+package com.rakun.weatherapp.controller;
+
+import com.rakun.weatherapp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/weather")
+@RequiredArgsConstructor
+public class WeatherController {
+
+    private final UserService userService;
+
+
+}
