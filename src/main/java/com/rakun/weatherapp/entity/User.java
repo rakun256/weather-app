a99561b744887f88cffba661bab1b5a1b0b6d5fe
rakun256 <emreuslu256@gmail.com>
2025-05-16 23:15:56 +0300

    Role made as ENUM and User Entity finished.

--- src/main/java/com/rakun/weatherapp/entity/User.java
+++ src/main/java/com/rakun/weatherapp/entity/User.java
@@ -1,4 +1,39 @@
 package com.rakun.weatherapp.entity;
 
+import com.rakun.weatherapp.util.Role;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id")
+    )
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<WeatherQuery> queries;
 }
